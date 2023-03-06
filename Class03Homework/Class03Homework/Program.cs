6036001acd826cbc7f83dbc732b6a6bacc1d816c
Borche Stojkoski <106525615+borrison-s@users.noreply.github.com>
2023-03-06 22:49:37 +0100

    Add files via upload

---
+++ Class03Homework/Class03Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("=== Task one - Sum of even ===");
+int[] numbersArray = new int[] { 4, 3, 7, 3, 2, 8 };
+
+int sum = 0;
+
+foreach (int number in numbersArray)
+{
+    if (number % 2 == 0)
+    {
+        sum += number;
+    }
+}
+Console.WriteLine("The result is: " +  sum);
---
+++ Class03Homework/StudentGroup/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("=== Task two - Student Group ===");
+string[] studentsG1 = new string[] { "Andrej", "Stefan", "Matej", "Ivan", "Aleksandar" };
+string[] studentsG2 = new string[] { "Goran", "Damjan", "Anastasija", "Simona", "Andrea" };
+
+Console.WriteLine("Enter student group: (There are 1 and 2)");
+string userInput = Console.ReadLine();
+
+int studentGroup;
+
+bool success = int.TryParse(userInput, out studentGroup);
+
+if (success)
+{
+    if (studentGroup != 1 && studentGroup != 2)
+    {
+        Console.WriteLine("Invalid input");
+    }
+    else if (studentGroup == 1)
+    {
+        foreach (string student in studentsG1)
+        {
+            Console.WriteLine(student);
+        }
+    }
+    else if (studentGroup == 2)
+    {
+        foreach(string student in studentsG2)
+        {
+            Console.WriteLine(student);
+        }
+    }
+}

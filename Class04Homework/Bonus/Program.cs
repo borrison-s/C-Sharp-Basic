c32ef59883ef564bc0fdddf5c1cd6f8829d0f3c8
Boris Stojkoski <boris.stojkoski@gmail.com>
2023-03-08 21:54:15 +0100

    Added bonus

---
+++ Class04Homework/Bonus/Program.cs
@@ -0,0 +1,15 @@
+﻿//##Bonus Take a sentence as input and print its largest word.
+Console.WriteLine("=== Bonus ===");
+Console.WriteLine("Enter a sentence");
+string sentence = Console.ReadLine();
+string[] words = sentence.Split(" ");
+string longestWord = "";
+
+foreach (string word in words)
+{
+    if (word.Length > longestWord.Length)
+    {
+        longestWord = word;
+    }
+}
+Console.WriteLine($"The longest word in the sentence is: {longestWord}");
